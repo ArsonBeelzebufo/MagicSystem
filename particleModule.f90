3d7046f8937fb21248470f98aa70011e458df132
cassiopeia3 <115739101+cassiopeia3@users.noreply.github.com>
2025-02-27 14:16:45 -0500

    Create particleModule.f90
    created a module containing the particle derived data type and some helpful methods, not done but my physics period is almost over

---
+++ particleModule.f90
@@ -0,0 +1,20 @@
+module particleModule
+  type particle
+    real,dimension(:,4)::position !x,y,z,t
+  end type particle
+
+  contains
+    int function search(p,t) !binary search
+      type(particle)::p
+      int::l,r
+      l=1
+      r=size(p%position)
+    end function search
+    
+    real,dimension(3) function derivative(p,n,t) !nth derivative of position over time
+      type(particle)::p
+      int::n
+      real::t
+      
+    end function derivative
+end module particleModule

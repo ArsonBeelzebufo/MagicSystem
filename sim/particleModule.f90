54c7abdbc0e493258243436a0ddb9258453a2ff1
cassiopeia3 <115739101+cassiopeia3@users.noreply.github.com>
2025-02-28 11:59:12 -0500

    Update particleModule.f90
    realized having arrays for position and time was not practical, also simulate helpers (derivatives,integrals,simulation) should be done on an array of types

--- sim/particleModule.f90
+++ sim/particleModule.f90
@@ -1,20 +1,25 @@
 module particleModule
   type particle
-    real,dimension(:,4)::position !x,y,z,t
+    real,dimension(3)::pos,v
+    real::t
   end type particle
 
   contains
-    integer function search(p,t) !binary search
-      type(particle)::p
-      int::l,r
+    function search(A,t)result(m) !binary search, returns the index that t would be inserted into position to keep the list sorted
+      real,dimension(:)::A
+      integer::l,r,m
       l=1
-      r=size(p%position)
+      r=size(A)
+      m=(l+r)/2
+      do while(l<r)
+        if(A(m)<t)then
+          l=m+1
+        else if(A(m)>t)then
+          r=m
+        else
+          return
+        end if
+        m=(l+r)/2
+      end do
     end function search
-    
-    real,dimension(3) function derivative(p,n,t) !nth derivative of position over time
-      type(particle)::p
-      int::n
-      real::t
-      
-    end function derivative
 end module particleModule

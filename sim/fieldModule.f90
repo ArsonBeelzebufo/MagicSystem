91ac20af99777c0eadc58df5fb22696c63bf9ef0
cassiopeia3 <115739101+cassiopeia3@users.noreply.github.com>
2025-03-04 09:26:04 -0500

    made strength function
    could be better? maybe using norms or matrix mult but these are either not implemented in fortran or not fast, potentially... also don't know about freduced, could probably be better.

--- sim/fieldModule.f90
+++ sim/fieldModule.f90
@@ -3,12 +3,26 @@ module fieldModule
   implicit none
 
   type(particle),dimension(:)::field
+  real::epsilon,sigma
 
   contains
     function strength(p)
-      real,dimension(3)::strength
+      real,dimension(3)::strength,pos
+      strength=(/0,0,0/)
       integer::p
-      
+      pos=field(p)
+      do i=1,field.size
+        if(i/=p)then
+          real,dimension(3)::d,other
+          real::r,FReduced,temp
+          other=field(i,:)
+          d=other-pos
+          r=dot_product(d,d)**.5
+          temp=(sigma/r)**6
+          FReduced=48*epsilon*temp*(temp-.5)/(r**2)
+          strength=strength+other*FReduced
+        end if
+      end do
     end function strength
   
     subroutine simulate(dt)

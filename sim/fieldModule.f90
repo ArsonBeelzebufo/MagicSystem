1d5c72573061fb043baf644f6315e62eefb7ce8c
cassiopeia3 <115739101+cassiopeia3@users.noreply.github.com>
2025-03-04 09:48:54 -0500

    Update fieldModule.f90

--- sim/fieldModule.f90
+++ sim/fieldModule.f90
@@ -2,21 +2,21 @@ module fieldModule
   use particleModule
   implicit none
 
-  type(particle),dimension(:)::field
-  real::epsilon,sigma
+  real::epsilon,sigma,t,n,ep,c,k
+  type(particle),dimension(n)::field
 
   contains
     function strength(p)
-      real,dimension(3)::strength,pos
+      real,dimension(3)::strength,position
       strength=(/0,0,0/)
       integer::p
-      pos=field(p)
-      do i=1,field.size
+      position=field(p)%pos
+      do i=1,n
         if(i/=p)then
           real,dimension(3)::d,other
           real::r,FReduced,temp
-          other=field(i,:)
-          d=other-pos
+          other=field(i)%pos
+          d=other-position
           r=dot_product(d,d)**.5
           temp=(sigma/r)**6
           FReduced=48*epsilon*temp*(temp-.5)/(r**2)
@@ -24,8 +24,39 @@ module fieldModule
         end if
       end do
     end function strength
-  
-    subroutine simulate(dt)
+
+    function bigStrength()
+      real,dimension(n,3)::bigStrength
+      integer::i
+      do i=1,n
+        bigStrength(i,:)=strength(i)
+      end do
+    end function bigStrength
+    
+    subroutine smallSimulate(dt)
       real::dt
-    end subroutine simulate
+      real,dimension(n,3)::bs
+      bs=bigStrength()
+      integer::i
+      do i=1,n
+        type(particle)::p
+        real::m
+        real,dimension(3)::F
+        F=bs(i)
+        m=k*(1-p%PE)/c**2
+        p=field(i)
+        p%pos=p%pos+p%v*dt+.5*dt**2*F/m
+        p%v=p%v+dt/m*F
+      end do
+      t=t+dt
+    end subroutine smallSimulate
+
+    subroutine bigSimulate(bigDt,gen)
+      real::bigDt,dt
+      integer::gen,i
+      dt=bigDt/gen
+      do i=1,gen
+        smallSimulate(dt)
+      end do
+    end subroutine bigSimulate
 end module fieldModule

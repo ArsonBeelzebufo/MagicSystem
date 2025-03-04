4b859d6f663b52098f16683c572a082da7238e32
cassiopeia3 <115739101+cassiopeia3@users.noreply.github.com>
2025-03-04 09:54:51 -0500

    Update particleModule.f90

--- sim/particleModule.f90
+++ sim/particleModule.f90
@@ -1,4 +1,6 @@
 module particleModule
+  implicit none
+
   type particle
     real,dimension(3)::pos,v
     real::PE !PE=percent energy; amount of intrinsic energy divided by the energy constant (in decimal not percent)

8d828076fb56c2f023664217384441081cd08465
cassiopeia3 <115739101+cassiopeia3@users.noreply.github.com>
2025-03-04 09:39:43 -0500

    Update particleModule.f90

--- sim/particleModule.f90
+++ sim/particleModule.f90
@@ -1,7 +1,7 @@
 module particleModule
   type particle
     real,dimension(3)::pos,v
-    real::PE !PE=percent energy; amount of intrinsic energy divided by the energy constant
+    real::PE !PE=percent energy; amount of intrinsic energy divided by the energy constant (in decimal not percent)
   end type particle
 
   contains

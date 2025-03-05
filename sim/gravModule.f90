f297c06af80997e5b769b0f39afdc526ef54e3bc
cassiopeia3 <115739101+cassiopeia3@users.noreply.github.com>
2025-03-05 09:38:07 -0500

    Create gravModule.f90

---
+++ sim/gravModule.f90
@@ -0,0 +1,11 @@
+module gravModule
+  use fieldModule
+  implicit none
+
+  contains
+    function grav(p)
+    end function grav
+
+    function bigGrav()
+    end function bigGrav
+end module gravModule

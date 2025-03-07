958d8134d57e8b3279377d1e6d12e059fb380c01
cassiopeia3 <115739101+cassiopeia3@users.noreply.github.com>
2025-03-07 12:46:03 -0500

    Update gravModule.f90

--- sim/gravModule.f90
+++ sim/gravModule.f90
@@ -1,11 +1,36 @@
-module gravModule
+@module gravModule
   use fieldModule
   implicit none
 
+  real::G
+
   contains
     function grav(p)
+      real,dimension(3)::grav,position
+      grav=(/0,0,0/)
+      integer::p
+      type(particle)::part
+      part=field(p)
+      position=part%pos
+      do i=1,n
+        if(i/=p)then
+          real,dimension(3)::d,other
+          real::r,FReduced
+          type(particle)::other
+          other=field(i)
+          d=other%pos-position
+          r=dot_product(d,d)**.5
+          FReduced=G*(k*(1-part%PE)/c**2)*(k*(1-other%PE)/c**2)/r**3
+          grav=grav+d*FReduced
+        end if
+      end do
     end function grav
-
+    
     function bigGrav()
+      real,dimension(n,3)::bigGrav
+      integer::i
+      do i=1,n
+        bigGrav(i,:)=grav(i)
+      end do
     end function bigGrav
 end module gravModule
